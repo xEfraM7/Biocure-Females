0cb72210f226dcc3083c8312e65fc5e096e1d0e6
xEfraM7 <efraincabrera35@gmail.com>
2026-01-15 19:02:19 -0400

    feat: se implemento el script de meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins, Open_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 import { SmoothScroll } from "@/components/common";
@@ -34,7 +35,32 @@ export default function RootLayout({
                 {/* YouTube preconnect (for video embed) */}
                 <link rel="preconnect" href="https://www.youtube.com" />
                 <link rel="preconnect" href="https://i.ytimg.com" />
+                {/* Meta Pixel noscript fallback */}
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: "none" }}
+                        src="https://www.facebook.com/tr?id=1169446995352011&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
             </head>
+            {/* Meta Pixel Script */}
+            <Script id="meta-pixel" strategy="afterInteractive">
+                {`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '1169446995352011');
+                    fbq('track', 'PageView');
+                `}
+            </Script>
             <body className={`antialiased ${poppins.variable} ${openSans.variable}`}>
                 <SmoothScroll>
                     <Providers>{children}</Providers>
